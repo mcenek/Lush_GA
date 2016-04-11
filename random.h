4bddfcde299a861a2ea012a747e1d428e8419d56
Martin Cenek <mcenek@darwin.(none)>
2016-04-11 09:49:33 -0800

    initial commit

---
+++ random.h
@@ -0,0 +1,13 @@
+#ifndef RANDOM_H_
+#define RANDOM_H_
+
+//long idum;
+
+long set_seed(long);
+float uniform();
+float normal();
+float expdev();
+int rnm(int, int);
+int r0n(int);
+long seed_random(long);
+#endif /*RANDOM_H_*/
